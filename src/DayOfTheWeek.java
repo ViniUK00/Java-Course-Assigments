d3cf2d81a96c411b3f3af6624fbefd0d8e616f33
ViniUK00 <ervin.bilo@yahoo.com>
2023-02-22 12:31:46 +0000

    added DayOfTheWeek.java

--- src/DayOfTheWeek.java
+++ src/DayOfTheWeek.java
@@ -2,21 +2,21 @@ public class DayOfTheWeek {
     public static void main(String[] args) {
         printDayOfWeek(0);
         printDayOfWeek(1);
-        printDayOfWeek(2);
-        printDayOfWeek(9);
+        printDayOfWeek(7);
     }
 
     // Enhanced switch statement
     public static void printDayOfWeek(int day) {
-        switch (day){
-            case 0 -> System.out.println("Sunday");
-            case 1 -> System.out.println("Monday");
-            case 2 -> System.out.println("Tuesday");
-            case 3 -> System.out.println("Wednesday");
-            case 4 -> System.out.println("Thursday");
-            case 5 -> System.out.println("Friday");
-            case 6 -> System.out.println("Saturday");
-            default -> System.out.println("Invalid Input");
-        }
+        String dayOfWeek = switch (day){
+            case 0 -> { yield "Sunday"; }
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid Input";
+        };
+        System.out.println(dayOfWeek);
     }
 }
