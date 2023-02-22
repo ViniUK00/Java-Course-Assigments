4427ddb9a8b5d3ac2df633b34f68957424c42625
ViniUK00 <ervin.bilo@yahoo.com>
2023-02-22 13:58:24 +0000

    added primeCounter in PrimeNumberChallenge.java

--- src/PrimeNumberChallenge.java
+++ src/PrimeNumberChallenge.java
@@ -6,6 +6,8 @@ public class PrimeNumberChallenge {
         System.out.println("3 is " + ((isPrime(3) ? "" : "NOT") + " prime number"));
         System.out.println("4 is " + ((isPrime(4) ? "" : "NOT") + " prime number"));
         System.out.println("17 is " + ((isPrime(17) ? "" : "NOT") + " prime number"));
+        System.out.println(primeCounter(999));
+        System.out.println(primeCounter(2));
 
 
     }
@@ -21,5 +23,19 @@ public class PrimeNumberChallenge {
             }
         }
         return true;
+    }
+
+    public static int primeCounter(int wholeNumber) {
+        int counter = 0;
+        for (int i=0; i <= wholeNumber; i++) {
+            if (isPrime(i)) {
+                counter++;
+            }
+            if (counter==3){
+                System.out.println("Found 3 - exiting for loop");
+                break;
+            }
+        }
+        return counter;
     }
 }
